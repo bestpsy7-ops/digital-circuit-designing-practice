3866e442c562dc492abb0445504901a0ca59bd37
bestpsy7-ops <bestpsy7@gmail.com>
2026-05-17 04:08:32 +0900

    Add files via upload

---
+++ day3-3.v
@@ -0,0 +1,18 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+	wire p1abc;
+    wire p1def;
+    wire p2ab;
+    wire p2cd;
+	
+    assign p1abc = p1a&p1b&p1c;
+    assign p1def = p1d&p1e&p1f;
+    assign p2ab = p2a&p2b;
+    assign p2cd = p2c&p2d;
+    assign p1y = p1abc|p1def;
+    assign p2y = p2ab|p2cd;
+
+endmodule
